0b4a0c767a0850a4dddaa7a00b57957aaae33599
YrystyNauryzbay <yrystty@bk.ru>
2025-01-20 20:51:05 +0500

    update all labs

---
+++ lab1/StringsFormat.py
@@ -0,0 +1,6 @@
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
---
+++ lab1/StringsModify.py
@@ -0,0 +1,5 @@
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.upper())
