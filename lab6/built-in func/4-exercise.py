a2f4fd40a29c6de1e540501d4c9388b4f70d3cc7
YrystyNauryzbay <yrystty@bk.ru>
2025-03-17 21:17:38 +0500

    update

--- lab6/built-in func/4-exercise.py
+++ lab6/built-in func/4-exercise.py
@@ -4,7 +4,7 @@ import time
 number = int(input("Enter the number to find the square root of: "))
 delay_ms = int(input("Enter delay in milliseconds: "))
 
-time.sleep(delay_ms / 1000)  # Convert milliseconds to seconds
+time.sleep(delay_ms / 1000) 
 result = math.sqrt(number)
 
 print(f"Square root of {number} after {delay_ms} milliseconds is {result}")
--- lab6/built-in func/5-exercise.py
+++ lab6/built-in func/5-exercise.py
@@ -1,10 +1,3 @@
-import math
-import time
-
-number = int(input("Enter the number: "))
-delay_ms = int(input("Enter delay in milliseconds: "))
-
-time.sleep(delay_ms / 1000)  # Convert milliseconds to seconds
-result = math.sqrt(number)
-
-print(f"Square root of {number} after {delay_ms} milliseconds is {result}")
+size_mylist=input("Enter  mylist:")
+mylist=list(map(int,size_mylist.split()))
+print("all True mylist:",all(mylist))
